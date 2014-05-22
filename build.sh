c3423fc6ae9546864b5f2167ea08e59628b26bc1
Ihor Burlachenko <ihor.burlachenko@gmail.com>
2014-05-22 22:29:44 +0300

    Fixed install script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 rm -rf ext
 zephir compile
 ./install.sh
--- install.sh
+++ install.sh
@@ -14,7 +14,6 @@ make
 make install
 
 exists=`php -m | grep ^ouchbase`
-echo $exists
 if [ -z $exists ]; then
     echo "extension=ouchbase.so" >> `php --ini | grep Loaded | cut -d" " -f12`
 fi
