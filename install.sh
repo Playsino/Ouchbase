b92f09c904c01fdbe7daa1b81e71645da68bae92
Ihor Burlachenko <ihor.burlachenko@gmail.com>
2014-05-22 22:22:36 +0300

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-cd ext
+if [ -h $0 ]; then
+    realpath="`dirname $0`/`readlink $0`"
+    curdir=`dirname $realpath`
+else
+    curdir=`dirname $0`
+fi
+
+cd $curdir/ext
 sudo phpize
 ./configure
 make
